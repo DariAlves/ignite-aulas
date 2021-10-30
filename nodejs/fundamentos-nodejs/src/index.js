871194b4dc15725a51a89e53406648c4ef73d93e
Dari Alves <darieltondf@gmail.com>
2021-10-29 21:27:15 -0300

    criar estrutura do projeto

---
+++ nodejs/fundamentos-nodejs/src/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+const port = 3333;
+
+app.get('/', (request, response) => {
+    return response.json({ message: "Hello world!" });
+});
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+})
