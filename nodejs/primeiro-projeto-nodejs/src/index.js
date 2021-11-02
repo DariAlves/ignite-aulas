4d600565b9c7ab14c80a9ec84abd828c4963d865
Dari Alves <darieltondf@gmail.com>
2021-11-02 19:33:12 -0300

    criar saque na conta

--- nodejs/primeiro-projeto-nodejs/src/index.js
+++ nodejs/primeiro-projeto-nodejs/src/index.js
@@ -24,6 +24,18 @@ function verifyIfExistsAccountCPF(request, response, next) {
     return next();
 }
 
+function getBalance(statement) {
+    const balance = statement.reduce((acc, operation) => {
+        if (operation.type === 'credit') {
+            return acc + operation.amount;
+        } else {
+            return acc - operation.amount;
+        }
+    }, 0);
+
+    return balance
+}
+
 // Deve ser possível criar uma conta
 app.post('/account', (request, response) => {
     const { cpf, name } = request.body;
@@ -48,8 +60,6 @@ app.post('/account', (request, response) => {
     return response.status(201).send();
 });
 
-// app.use(verifyIfExistsAccountCPF);
-
 // Deve ser possível buscar o extrato bancário do cliente
 app.get('/statement', verifyIfExistsAccountCPF, (request, response) => {
     // Não deve ser possível buscar extrato em uma conta não existente
@@ -66,14 +76,37 @@ app.post('/deposit', verifyIfExistsAccountCPF, (request, response) => {
 
     const { customer } = request;
 
-    const statementOperations = {
+    const statementOperation = {
         description,
         amount,
         created_at: new Date(),
         type: 'credit'
     }
 
-    customer.statement.push(statementOperations)
+    customer.statement.push(statementOperation)
+
+    return response.status(201).send();
+});
+
+// Deve ser possível realizar um saque
+app.post('/withdraw', verifyIfExistsAccountCPF, (request, response) => {
+    const { amount } = request.body;
+    const { customer } = request;
+
+    const balance = getBalance(customer.statement);
+
+    // Não deve ser possível fazer saque quando o saldo for insuficiente
+    if (balance < amount) {
+        return response.status(400).json({ error: "Insufficient funds!" });
+    }
+
+    const statementOperation = {
+        amount,
+        created_at: new Date(),
+        type: 'debit'
+    };
+
+    customer.statement.push(statementOperation);
 
     return response.status(201).send();
 });
