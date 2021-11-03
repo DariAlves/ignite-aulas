8f844565a1ad6602977516805c6de83f7f2d1658
Dari Alves <darieltondf@gmail.com>
2021-11-03 18:26:39 -0300

    adicionar os tipos

--- nodejs/chapter-02/01-aplicacoes/typescript/dist/server.js
+++ nodejs/chapter-02/01-aplicacoes/typescript/dist/server.js
@@ -4,11 +4,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
+var routes_1 = require("./routes");
 var app = (0, express_1.default)();
 var port = process.env.PORT || 3333;
-app.get('/', function (request, response) {
-    return response.json({ message: 'Hello, world!' });
-});
+app.get('/', routes_1.createCourse);
 app.listen(port, function () {
     console.log("Server listening on port " + port);
 });
--- nodejs/chapter-02/01-aplicacoes/typescript/src/server.ts
+++ nodejs/chapter-02/01-aplicacoes/typescript/src/server.ts
@@ -1,12 +1,11 @@
 import express from 'express';
+import { createCourse } from './routes';
 
 const app = express();
 
 const port = process.env.PORT || 3333;
 
-app.get('/', (request, response) => {
-    return response.json({ message: 'Hello, world!' });
-});
+app.get('/', createCourse);
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
