be780576fcc344fde665d51e35cd905464e63190
Dari Alves <darieltondf@gmail.com>
2021-11-07 17:26:33 -0300

    inserir tipagem para categoria

---
+++ nodejs/chapter-02/01-aplicacoes/rentalx/src/model/Category.ts
@@ -0,0 +1,17 @@
+import { v4 as uuidV4 } from 'uuid';
+
+class Category {
+  id?: string;
+  name: string;
+  description: string;
+  created_at: Date;
+
+  // A responsabilidade de criar o Id passa a ser da classe
+  constructor() {
+    if (!this.id) {
+      this.id = uuidV4();
+    }
+  }
+}
+
+export { Category };
