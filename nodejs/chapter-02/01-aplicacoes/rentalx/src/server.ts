5c1d20625b9e7e879a12a1e1b2b001d66e0565bf
Dari Alves <darieltondf@gmail.com>
2021-11-07 16:02:18 -0300

    debugar a aplicacao

--- nodejs/chapter-02/01-aplicacoes/rentalx/src/server.ts
+++ nodejs/chapter-02/01-aplicacoes/rentalx/src/server.ts
@@ -2,10 +2,17 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.json());
+
 const port = process.env.PORT || 3333;
 
 app.get('', (request, response) => {
   return response.json({ message: 'Hello, World!' });
+});
+
+app.post('/courses', (request, response) => {
+  const { name } = request.body;
+  return response.json({ name });
 });
 
 app.listen(port, () => {
